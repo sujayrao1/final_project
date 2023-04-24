b3c831910026dceb1ea324eebd756996fafd3689
sujayrao1 <112927455+sujayrao1@users.noreply.github.com>
2023-04-24 11:58:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -106,7 +106,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create VideoCall By ZEGOCLOUD UIKits</title>
+        <title>Video Meet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
